9154474af353e227be20cbb18b1934128558748a
John Allen <valerio.allen24@gmail.com>
2021-11-03 13:06:40 +1300

    Health and DamageOverTime
    Health script and DamageOverTime

---
+++ Assets/John/Script/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float HealthPoints = 100;
+    public void TakeDamageOverTime(float amount)
+    {
+        HealthPoints -= Time.deltaTime * amount;
+        if(HealthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
