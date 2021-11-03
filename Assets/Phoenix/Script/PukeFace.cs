39335ab11c328e38c4f178dd83b9423de68d56a3
Phoenix <phoenix.millar.020@gmail.com>
2021-11-03 15:16:49 +1300

    AI Chases Player Tagged Object
    Ai looks at the Player as it chases them, even through walls needs more tweaking

---
+++ Assets/Phoenix/Script/PukeFace.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PukeFace : AiBehaviour
+{
+    private PukeFace ai;
+    private float threshold;
+
+    [SerializeField] private float startingHP;
+    private float currentHP;
+
+    private void Start()
+    {
+        currentHP = startingHP;
+    }
+
+    public float GetCurrentHP()
+    {
+        return currentHP;
+    }
+}
