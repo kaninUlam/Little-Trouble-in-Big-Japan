b683d7868ea7fd141b436a6539ad282d166ba13e
Phoenix <phoenix.millar.020@gmail.com>
2021-11-04 14:51:59 +1300

    Ammo and Health Objs

--- Assets/Phoenix/Script/CommonAi.cs
+++ Assets/Phoenix/Script/CommonAi.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CommonAi : AiBehaviour
 {
-    [SerializeField] private float startingHP;
-    [SerializeField] private float lowHP;
-    private float currentHP;
+    [SerializeField] private float startingHP = 100;
+    private float currentHP = 100;
+
+    public float damageDealt;
+
+    Health playerHP;
 
     private void Start()
     {
         currentHP = startingHP;
+        playerHP = Player.GetComponent<Health>();
+        StartCoroutine(attackDelay());
+    }
+
+    IEnumerator attackDelay()
+    {
+        yield return new WaitForSeconds(5);
     }
 
     public float GetCurrentHP()
     {
         return currentHP;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHP.takeDamage(damageDealt);
+            
+            //if (playerHP != null)
+            //{
+            //    playerHP.takeDamage(10f);
+            //}
+        }
     }
 }
