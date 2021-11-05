bcf84ed88cfd62bb7fa22ddddfa9f938c61ea3cb
Phoenix <phoenix.millar.020@gmail.com>
2021-11-05 13:43:55 +1300

    Spawns in Health pack on Death
    Made changes to script AI Script, it can spawn in health pack on death. This will be changed just wanted to try something

--- Assets/Phoenix/Script/dropItem.cs
+++ Assets/Phoenix/Script/dropItem.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class dropItem : MonoBehaviour
 {
-    public GameObject[] ammoTypes; //Ammo Storage
-    private int ammoNum; //Random Ammo Picker
-    private int randNum; //Random number designation
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
