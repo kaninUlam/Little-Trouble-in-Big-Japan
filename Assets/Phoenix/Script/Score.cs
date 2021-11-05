733e66b7cce1f46c9c781e0b00122f898ac11082
Phoenix <phoenix.millar.020@gmail.com>
2021-11-05 15:12:08 +1300

    Save

---
+++ Assets/Phoenix/Script/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public float score = 0;
+
+    [SerializeField] Text scoreAmount;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        score = 0;
+    }
+
+    public void AddScore(float amount)
+    {
+        score += amount;
+    }
+
+    private void UpdateScoreUI()
+    {
+        scoreAmount.text = score.ToString("0");
+    }
+}
