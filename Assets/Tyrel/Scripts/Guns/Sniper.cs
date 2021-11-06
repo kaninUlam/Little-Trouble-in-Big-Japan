70cadb7a512faebc6ae8261de2a57aa04d59649d
xyndall <tyrel.ratima@gmail.com>
2021-11-07 09:54:33 +1300

    ammo WIP
    ammo work in progress
    expect another later

--- Assets/Tyrel/Scripts/Guns/Sniper.cs
+++ Assets/Tyrel/Scripts/Guns/Sniper.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Sniper : GunFire
 {
+
+    public SniperAmmo SAmmo;
+
     public float fireRate = 0.5f;
 
     public float nextFire;
 
+    
 
-
+    private void Start()
+    {
+        SAmmo.GetComponent<SniperAmmo>();
+    }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire)
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire && SAmmo.ammo > 0)
         {
             FireAssualtProjectile();
             nextFire = Time.time + fireRate;
---
+++ Assets/Tyrel/Scripts/Guns/SniperAmmo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperAmmo : MonoBehaviour
+{
+
+    public float ammo = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            ammo += 20;
+        }
+    }
+
+
+}
