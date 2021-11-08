8aa75020743d31763f5e6fbfdfe1a65b7a6518f3
tyrel <tyrel.ratima@gmail.com>
2021-11-08 15:23:30 +1300

    perks
    interactable perks that change speed and fire rate :)

--- Assets/Tyrel/Scripts/Guns/AssualtRifle.cs
+++ Assets/Tyrel/Scripts/Guns/AssualtRifle.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class AssualtRifle : GunFire
 {
-    public float fireRate = 0.25f;
-
+    public float fireRate = 0.5f;
+    float normalFireRate = 0.5f;
     public float nextFire;
 
-    
 
-   
+    private void Start()
+    {
+        fireRate = normalFireRate;
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire)
