814aa2504e82f00298f0e2d3b4de7b5f4c21d9fc
Phoenix <phoenix.millar.020@gmail.com>
2021-11-11 12:14:34 +1300

    Hyogens Script Update

--- Assets/Phoenix/Script/dropItem.cs
+++ Assets/Phoenix/Script/dropItem.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class dropItem : MonoBehaviour
 {
-    public int[] table =
+    [SerializeField]
+    private GameObject[] itemList;
+    private int itemNum;
+    private int randNum;
+    private Transform Epos;
+
+    private void Start()
     {
-        70, //
-        30, //
-        30, //
-        30 //
-    };
+        Epos = GetComponent<Transform>();
+        Debug.Log(itemList);
+    }
+
+    public void itemDrop()
+    {
+        randNum = Random.Range(0, 101);
+        Debug.Log("Random Number is " + randNum);
+
+        if (randNum >= 90)
+        {
+            itemNum = 3;
+            Instantiate(itemList[itemNum], Epos.position, Quaternion.identity);
+        }
+        else if (randNum > 70 && randNum < 90)
+        {
+            itemNum = 2;
+            Instantiate(itemList[itemNum], Epos.position, Quaternion.identity);
+        }
+        else if (randNum > 40 && randNum < 70)
+        {
+            itemNum = 1;
+            Instantiate(itemList[itemNum], Epos.position, Quaternion.identity);
+        }
+    }
 }
