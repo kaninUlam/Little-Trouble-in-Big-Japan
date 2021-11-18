65b37facda70681c8fb2572d52f9701bf5d820a1
Phoenix <phoenix.millar.020@gmail.com>
2021-11-18 13:07:29 +1300

    Drop Chance WIP

--- Assets/Phoenix/Script/EnemyHealth.cs
+++ Assets/Phoenix/Script/EnemyHealth.cs
@@ -10,33 +10,32 @@ public class EnemyHealth : MonoBehaviour
     public float MaxEnemyHealth = 200;
     public float Points = 0;
 
-    public const float m_dropChance = 1f / 10f;
+    // Drop Chance
+    public const float _dropRate = 1 / 8;
+    public GameObject[] dropItem;
 
     //From pointSystem Script it will be called uiManager
     //public pointSystem points;
 
-    public GameObject dropItem;
-
-    // Start is called before the first frame update
     void Start()
     {
-        MaxEnemyHealth = enemyHealth;
+        MaxEnemyHealth = enemyHealth; // Enemy HP
         //_uiManager = GameObject.Find("Canvas").GetComponent<pointSystem>();
     }
-
+   
     public void takeDamage(float amount)
     {
         enemyHealth -= amount;
 
         if (enemyHealth <= 0)
         {
-            if (Random.Range(0f, 1f) <= m_dropChance)
+            if (Random.Range(0, 1) <= _dropRate)
             {
-                Instantiate(dropItem, transform.position, Quaternion.identity);
+                int indexToDrop = Random.Range(0, dropItem.Length);
+                Instantiate(dropItem[indexToDrop], transform.position, Quaternion.identity);
             }
 
             pointSystem.Instance.UpdateScore(Points);
-
             Destroy(gameObject);
         }
     }
