114681037fb7dd02305ca9bad4169292c8621ccc
Phoenix <phoenix.millar.020@gmail.com>
2021-11-19 13:55:57 +1300

    Attempted to Add Staggering to Enemys
    Staggering is Unknown as no enemy that spawns in is Alive to see it take effect

--- Assets/Phoenix/Script/EnemyHealth.cs
+++ Assets/Phoenix/Script/EnemyHealth.cs
@@ -9,13 +9,15 @@ public class EnemyHealth : MonoBehaviour
     public float enemyHealth = 200;
     public float MaxEnemyHealth = 200;
     public float Points = 0;
-
+    // Drop Rate Percentage
     public const float _dropRate = 0.45f;
 
-    //From pointSystem Script it will be called uiManager
+    // From pointSystem Script it will be called uiManager
     //public pointSystem points;
 
+    // Drop Items
     public GameObject[] dropItem;
+    // Particle Effect
     public GameObject enemyDeathParticle;
 
     // Start is called before the first frame update
@@ -31,10 +33,10 @@ public class EnemyHealth : MonoBehaviour
 
         if (enemyHealth <= 0)
         {
-            if (Random.Range(0f, 1f) <= _dropRate)
+            if (Random.Range(0f, 1f) <= _dropRate) // Percentage to drop with the Rate
             {
-                int indexToDrop = Random.Range(0, dropItem.Length);
-                Instantiate(dropItem[indexToDrop], transform.position, Quaternion.identity);
+                int indexToDrop = Random.Range(0, dropItem.Length); // Drop Items
+                Instantiate(dropItem[indexToDrop], transform.position, Quaternion.identity); // What it Drops
             }
 
             pointSystem.Instance.UpdateScore(Points);
