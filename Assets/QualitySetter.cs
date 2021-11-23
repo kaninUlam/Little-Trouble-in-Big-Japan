a8a66dec85f8dc78c194b0062fc9f52d2086b510
tyrel <tyrel.ratima@gmail.com>
2021-11-24 11:02:19 +1300

    UpdatedMenuScreen
    MENUSCREEN

---
+++ Assets/QualitySetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QualitySetter : MonoBehaviour
+{
+    Dropdown _dropdown;
+
+    private void Start()
+    {
+        _dropdown = GetComponent<Dropdown>();
+        _dropdown.options.Clear();
+        string[] qualityLevels = QualitySettings.names;
+
+        for (int i = 0; i < qualityLevels.Length; i++)
+        {
+            Dropdown.OptionData data = new Dropdown.OptionData(qualityLevels[i]);
+            _dropdown.options.Add(data);
+        }
+
+        _dropdown.value = QualitySettings.GetQualityLevel();
+
+    }
+
+    public void ChangeQuality(int value)
+    {
+        QualitySettings.SetQualityLevel(value);
+    }
+}
