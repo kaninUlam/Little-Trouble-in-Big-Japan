b8ea8e6d863f86deadf9b7c951289a617992403c
John Allen <valerio.allen24@gmail.com>
2021-11-26 13:37:32 +1300

    Exit

---
+++ Assets/John/Script/Game/DoorScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public DifferentPointSystem _Score;
+    public GameObject Door;
+    public int ScoreNeeded;
+
+    bool playerInZone = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Score = GetComponent<DifferentPointSystem>();
+    }
+    private void Update()
+    {
+        if(playerInZone == true && Input.GetKeyDown(KeyCode.E))
+        {
+            if(_Score._PlayerScore>= 10000)
+            {
+                OpenDoor();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            playerInZone = true;
+        }    
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            playerInZone = false;
+        }
+    }
+
+    void OpenDoor()
+    {
+        Door.SetActive(false);
+    }
+}
---
+++ Assets/John/Script/Game/EndGameScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameScript : MonoBehaviour
+{
+    bool PlayerNearLadder = false;
+
+    public DifferentPointSystem _currentscore;
+    private void Update()
+    {
+        if(PlayerNearLadder == true && Input.GetKeyDown(KeyCode.E))
+        {
+            if(_currentscore._PlayerScore >= 10000)
+            {
+                loadnextScene();
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player near Ladder");
+            PlayerNearLadder = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Left Ladder");
+            PlayerNearLadder = false;
+        }
+    }
+    void loadnextScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
