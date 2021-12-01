06d50f59025e1a7b6646b65f8a9eda3b9e4ea244
John Allen <valerio.allen24@gmail.com>
2021-12-01 13:13:11 +1300

    Objectives
    Objectives and End Game

---
+++ Assets/John/Script/Game/EndGameCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameCheck : MonoBehaviour
+{
+    public GameObject wincollider;
+    public CheckForObjectives completed; 
+    // Update is called once per frame
+    void Update()
+    {
+        if(completed.allperksareactive == true)
+        {
+            wincollider.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (completed.allperksareactive == true)
+            {
+                wincollider.SetActive(true);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (completed.allperksareactive == true)
+            {
+                wincollider.SetActive(false);
+            }
+        }
+    }
+}
