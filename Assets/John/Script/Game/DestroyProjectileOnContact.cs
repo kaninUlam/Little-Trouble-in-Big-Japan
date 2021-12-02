6e010b47e5538b5ced40cbf2a54610d833d2e84f
John Allen <valerio.allen24@gmail.com>
2021-12-02 14:35:31 +1300

    New Damage Room

---
+++ Assets/John/Script/Game/DestroyProjectileOnContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyProjectileOnContact : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy" && collision.gameObject.tag == "Cold")
+        {
+            Destroy(this);
+        }
+    }
+}
